33076cfbcae5883449f81edc23154b0b682e11dc
ZaitsevaPOLINA <ZPoolly@gmail.com>
2023-10-31 13:53:44 +0400

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+middle=int(len(list_players)/2)
+# TODO Разделите участников на две команды
+team_A=list_players[0:middle]
+team_B=list_players[middle:]
+print(team_A)
+print(team_B)
